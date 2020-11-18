307e8623fd294fa47d49407bf309fc81ebbb619d
Anna Udziela <annaudziela@gmail.com>
2020-11-18 13:40:20 +0100

    public img added

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,54 @@
 import '../scss/main.scss';
 
 
-const name = 'Ania';
-console.log(name);
+fetch('https://api.github.com/users/annaudziela/repos?sort=created&direction=asc')
+.then((res) => res.json())
+.then((res) => {
+  const gridBox = document.querySelector('.projects-grid--js');
+  for (let repo of res) {
+    const {description, homepage, html_url, name} = repo;
+const template = ` <article class="project">
+<div class="project__beam">
+  <div class="project__dot"></div>
+  <div class="project__dot"></div>
+  <div class="project__dot"></div>
+</div>
+<div class="project__content">
+  <img class="project__image" src="public/img/Github Icon Big.svg">
+<h3 class="project__grid project__title">
+<span class="project__label">project:</span> <span class="project__title--special">${name}</span>
+</h3>
+<p class="project__grid">
+  <span class="project__label">description:</span
+    ><span>${description}</span>
+</p>
+<p class="project__grid project__grid--special">
+  <span class="project__label">demo:</span
+    ><span
+    >&lt;<a class="project__link" 
+    href="${homepage}" 
+    title="${name}">see_here</a
+      >&gt;</span
+      >
+</p>
+<p class="project__grid">
+  <span class="project__label">github:</span>
+  <span>
+    &lt;<a class="project__link" 
+    href="${html_url}" 
+    title="${name}">source_code</a>&gt;
+  </span>
+</p>
+</div>
+</article> `;
 
-// fetch('https://api.github.com/users/annaudziela/repos?sort=created&direction=asc')
-// .then(resp => resp.json())
-// .then(resp => {
-//     for (let repo of resp) {
-//     const {name, html_url} = repo;
-//     const repositoryList = document.querySelector('.project__link--js');
-//     const myTemplate = `<li>
-//     ${name} || <a href="${html_url}"> >see_here</a
-//     >&gt; </a>
-//     </li>`;
-//     repositoryList.innerHTML += myTemplate;
-// }
-// })
-// .catch(error => {
-//     console.log('Error: unable to download repos');
-// })
+gridBox.innerHTML += template;
+
+  }
+})
+.catch((e) => console.log(e));
+
+
+
 
 
